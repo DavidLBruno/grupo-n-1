fc5f1b18d12c784808a1f68b19b495ef20a1d420
DiegoBorja09 <diegoborja09@gmail.com>
2022-11-03 01:25:33 -0500

    add delete and update routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const { create } = require('../controllers/users')
+const express = require('express')
+const { deleteU,create, update} = require('../controllers/users')
 
 const router = express.Router();
 
 router.post("/create",create)
+router.put("/update/:id",update)
+router.delete("/delete/:id",deleteU)
+
 
 
 module.exports = router
