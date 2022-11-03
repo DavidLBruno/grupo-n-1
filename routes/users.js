0d262f21ef376e62f377728aa7f39d8e4743dd08
DiegoBorja09 <diegoborja09@gmail.com>
2022-11-03 02:40:31 -0500

    creation of middlewares for data validation

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const { deleteU,create, update} = require('../controllers/users')
+const { validateCreate } = require('../middlewares/index')
 
 const router = express.Router();
 
-router.post("/create",create)
+router.post("/create",validateCreate,create)
 router.put("/update/:id",update)
 router.delete("/delete/:id",deleteU)
 
