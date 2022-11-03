3d591143e9c72faac13d07702a0ad12a01303c22
DiegoBorja09 <diegoborja09@gmail.com>
2022-11-03 17:16:28 -0500

    cambios

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,9 @@ const { validateCreate } = require('../middlewares/index')
 
 const router = express.Router();
 
+
+router.get("/",list)
+router.get("/:id",detail)
 router.post("/create",validateCreate,create)
 router.put("/update/:id",update)
 router.delete("/delete/:id",deleteU)
