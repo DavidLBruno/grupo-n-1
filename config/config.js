909bacf2d46993f814847cda39b9b31537791c60
DiegoBorja09 <diegoborja09@gmail.com>
2022-11-08 12:16:50 -0500

    adding login and authentication

--- config/config.js
+++ config/config.js
@@ -2,19 +2,26 @@ require('dotenv').config()
 
 module.exports = {
   development: {
+    production:process.env.NODE_ENV==="production",
+    development:process.env.NODE_ENV==="development",
+    jwtSecret: process.env.JWT_SECRET,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql',
+    
   },
   production: {
+    production:process.env.NODE_ENV==="production",
+    development:process.env.NODE_ENV==="development",
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
+    jwtSecret:process.env.JWT_SECRET,
     dialect: 'mysql',
   },
 }
