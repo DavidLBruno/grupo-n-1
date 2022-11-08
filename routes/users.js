a5b9bc708c5034c9d31aa31e072ab07159206b5b
bruno <br1_moralesotmail.com>
2022-11-08 17:58:14 -0300

    challenge rama devBruno

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const { deleteU, create, update, list,detail, servicioimagenpost, login} = require('../controllers/users')
-const { validateCreate,validateToken } = require('../middlewares/index')
+
+const express = require('express');
+const { deleteU, create, update, list,detail, servicioimagenpost} = require('../controllers/users')
+const { validateCreate, validateToken } = require('../middlewares/index')
 
 const router = express.Router();
-
 
 
 router.get("/",validateToken, list)
 router.get("/:id",detail)
 router.post("/create",validateCreate,create)
-router.post("/login",login)
 router.put("/update/:id",update)
 router.delete("/delete/:id",deleteU)
+
 router.post("/imagen" ,servicioimagenpost);
 
 module.exports = router
