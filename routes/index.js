070c03837e72118ae7e835a88ceca293b12b9580
walter <walteralturoaparicio@gmail.com>
2022-11-08 16:37:27 -0500

    new documentation

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,16 @@ const categoriesRouter = require("./categories");
 const transactionRouter = require('./transactions');
 const router = express.Router();
 
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerJsDoc = YAML.load("./api.yaml");
+
+//Endpoints
 router.use("/user", usersRouter);
 router.use("/categories", categoriesRouter);
-
 router.use('/transactions', transactionRouter);
+
+//Documentacion
+router.use("/api-doc", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc))
 
 module.exports = router;
